aa05c58e812da91d6607d6f7843274e0fd43d7ac
Erik Lüth <erik.lueth@tum.de>
2019-05-07 18:36:24 +0200

    Interactive Ink Included
    Included Interactive Ink Java module

---
+++ android/app/src/main/kotlin/augmos/erik/augmos_test/IInkApplication.java
@@ -0,0 +1,23 @@
+// Copyright MyScript. All rights reserved.
+
+package augmos.erik.augmos_test;
+
+import android.app.Application;
+
+import com.myscript.iink.Engine;
+
+public class IInkApplication extends Application
+{
+  private static Engine engine;
+
+  public static synchronized Engine getEngine()
+  {
+    if (engine == null)
+    {
+      engine = Engine.create(MyCertificate.getBytes());
+    }
+
+    return engine;
+  }
+
+}
